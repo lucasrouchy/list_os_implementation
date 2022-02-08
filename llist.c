975e391a09eb2c67fff63baed349e5afd9938e59
Lucas <55973521+lucasrouchy@users.noreply.github.com>
2022-02-07 21:12:33 -0800

    corrected the strcmp to be it for insert head function and got print to work so I know its working now

--- llist.c
+++ llist.c
@@ -8,9 +8,11 @@ int main(int argc, char *argv[]){
   struct node *head = NULL;
   for (int i = 1; i <argc; i++){
     char *input = argv[i];
-    if (strcmp(input, "ih") == 0){
+    if (strcmp(input, "it") == 0){
       int num = atoi(argv[i+1]);
       llist_insert_head(&head, node_alloc(num));
+    } else if (strcmp(input, "p") == 0){
+      llist_print(head);
     }
   }
   return 0;
@@ -20,6 +22,18 @@ void llist_insert_head(struct node **head, struct node *n){
   struct node *old = *head;
   n->next = old;
   *head = n;
+}
+
+void llist_print(struct node *head){
+  struct node *ptr = head;
+  if(head == NULL) printf("[Empty]");
+  while (ptr != NULL) {
+    int current_value = ptr->value;
+    printf("%d", current_value);
+    if (ptr->next != NULL) printf(" -> ");
+    ptr = ptr->next;
+  }
+  printf("\n");
 }
 struct node *node_alloc(int value){
   struct node *head = malloc(sizeof(struct node));
